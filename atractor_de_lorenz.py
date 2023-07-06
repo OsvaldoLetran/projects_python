4b66ed720180031cbab8a79672c50bdf502cc424
OsvaldoLetran <oswaldo.letran@gmail.com>
2023-07-06 00:01:54 -0600

    atractor modified

--- atractor_de_lorenz.py
+++ atractor_de_lorenz.py
@@ -3,9 +3,11 @@ import numpy as np
 from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D
 
+
 rho = 28.0     # desde rho = 25 hasta rho = 90
 sigma = 10.0
 beta = 8.0 / 3.0
+
 
 def f(state, t):
     x, y, z = state  # Desempaqueta el vector de estado
@@ -14,10 +16,13 @@ def f(state, t):
 state0 = [1.0, 1.0, 1.0]
 t = np.arange(0.0, 40.0, 0.01)
 
+# odeint() resuelve EDOs de primer orden. syntax: odeint(func, valor_inicial, t)
+# devuelve una matriz
 states = odeint(f, state0, t)
 
 fig = plt.figure()
 # ax = fig.add_subplot(projection='3d')
-ax = plt.axes(projection='3d')
+ax = plt.axes(projection = '3d')
 ax.plot(states[:, 0], states[:, 1], states[:, 2])
+# states[:,0] selecciona todas las filas de la 1er columna de states
 plt.show()
