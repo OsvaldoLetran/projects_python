2541505d433aa4481291364dbc668db89456491b
OsvaldoLetran <oswaldo.letran@gmail.com>
2023-07-21 23:38:01 -0600

    modified guitar

--- guitar.py
+++ guitar.py
@@ -1,6 +1,6 @@
 class Guitar:
-    def __init__(self):    #constructor de clase
-        self.n_strings = 6
+    def __init__(self, n_strings):    #constructor de clase
+        self.n_strings = n_strings
 
 
     def play(self):
@@ -9,17 +9,17 @@ class Guitar:
 
 # INHERITANCE (child class) HERENCIA
 class ElectricGuitar(Guitar):
-    def __init__(self):
-        super().__init__()
-        self.n_strings = 8
+    def __init__(self, n_strings):
+        super().__init__(n_strings)
 
 
     def play_louder(self):
         return "pam pam pam pam pam".upper()
 
 
-my_guitar = ElectricGuitar()
+guitar = Guitar(6)
+my_guitar = ElectricGuitar(8)
 print("child class: ", my_guitar.n_strings)
 print("child class plays: ", my_guitar.play_louder())
-print("parent class: ", Guitar().n_strings)
-print("parent class plays: ", Guitar().play())
+print("parent class: ", guitar.n_strings)
+print("parent class plays: ", guitar.play())
